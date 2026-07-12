451b5f54a0cedeb3d9d00149f7247a33ebf6e52d
Arman Ansari <clasiygo@gmail.com>
2026-07-12 15:47:28 +0530

    Update Landing.js

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -141,6 +141,9 @@ export default function Landing() {
       <Link to="/contact-us" className="text-sm hover:text-orange-500">
         Contact Us
       </Link>
+      <Link to="/refund-policy" className="text-sm hover:text-orange-500">
+        Refund Policy
+      </Link>
     </div>
 
   </div>
